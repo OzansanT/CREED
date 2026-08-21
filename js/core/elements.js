b541ccc17d7132406f8c3611d762d64de2027be6
OzansanT <167488344+OzansanT@users.noreply.github.com>
2026-08-21 22:21:54 +0300

    Expose Source Control activity button

--- js/core/elements.js
+++ js/core/elements.js
@@ -12,6 +12,7 @@ export function getElements() {
     togglePrimarySidebarBtn: document.getElementById("togglePrimarySidebarBtn"),
     activityExplorerBtn: document.getElementById("activityExplorerBtn"),
     activitySearchBtn: document.getElementById("activitySearchBtn"),
+    activitySourceControlBtn: document.getElementById("activitySourceControlBtn"),
     activityRunBtn: document.getElementById("activityRunBtn"),
     toggleSecondarySidebarBtn: document.getElementById("toggleSecondarySidebarBtn"),
     secondarySidebar: document.getElementById("secondarySidebar"),
