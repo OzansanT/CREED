b6f231a3a1aef0006c3ede3ac2a27c221be99d35
OzansanT <167488344+OzansanT@users.noreply.github.com>
2026-08-30 20:55:23 +0300

    Remove Run and Debug DOM lookup

--- js/core/elements.js
+++ js/core/elements.js
@@ -12,7 +12,6 @@ export function getElements() {
     togglePrimarySidebarBtn: document.getElementById("togglePrimarySidebarBtn"),
     activityExplorerBtn: document.getElementById("activityExplorerBtn"),
     activitySearchBtn: document.getElementById("activitySearchBtn"),
-    activityRunBtn: document.getElementById("activityRunBtn"),
     toggleSecondarySidebarBtn: document.getElementById("toggleSecondarySidebarBtn"),
     secondarySidebar: document.getElementById("secondarySidebar"),
     toggleBottomPanelBtn: document.getElementById("toggleBottomPanelBtn"),
